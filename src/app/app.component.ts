4a7f7d1fa56aa83f2e678a970b6c8134bb9f0b3d
Mitch Okonta <mitch@kotano.com>
2025-01-17 21:01:46 +0000

    Add code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AudioPlayerComponent } from "../audio-player/audio-player.component";
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AudioPlayerComponent, NgbModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
